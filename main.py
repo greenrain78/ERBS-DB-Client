c80c5299787b41440967060b32d52f0a0b1aebde
daewon kim <kimdw010130@daum.net>
2021-01-20 15:21:48 +0900

    input log debug status

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from time import sleep
 
 from Log.infoLog import logger as log
 import MainScheduler
+from Settings import debug, on_MariaDB, on_sqlDB
 
 if __name__ == '__main__':
+    log.info('status debug: %s, mariaDB: %s, sqlDB: %s', debug, on_MariaDB, on_sqlDB)
     log.info('run __main__')
     MainScheduler.start()
 
+    log.info('run while')
     while True:
         log.debug("Running main process...............")
         sleep(10)
